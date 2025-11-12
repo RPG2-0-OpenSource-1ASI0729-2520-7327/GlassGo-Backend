d5e33c160f738b789b9aed994f946f94688e1301
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-12 16:39:53 -0500

    chore: initial commit.

---
+++ src/test/java/com/glassgo/platform/GlassgoPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glassgo.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlassgoPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

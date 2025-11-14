396eec5926062052ee1aee5fff363f51baa353ef
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-14 17:41:15 -0500

    feat(subscription-plan): add subscription plan repository.

---
+++ src/main/java/com/glassgo/platform/paymentsandsubscriptions/infrastructure/persistence/jpa/repositories/SubscriptionPlanRepository.java
@@ -0,0 +1,10 @@
+package com.glassgo.platform.paymentsandsubscriptions.infrastructure.persistence.jpa.repositories;
+
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.aggregates.SubscriptionPlan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlan, Long> {
+
+}

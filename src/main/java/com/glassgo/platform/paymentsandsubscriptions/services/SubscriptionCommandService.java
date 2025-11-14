da00c6ea82ad0f4a7e3e9c484a7f0c28f624fad8
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-14 17:40:05 -0500

    feat(subscription): add subscription command and query services.

---
+++ src/main/java/com/glassgo/platform/paymentsandsubscriptions/services/SubscriptionCommandService.java
@@ -0,0 +1,14 @@
+package com.glassgo.platform.paymentsandsubscriptions.services;
+
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.aggregates.Subscription;
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.commands.CreateSubscriptionCommand;
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.commands.DeleteSubscriptionCommand;
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.commands.UpdateSubscriptionCommand;
+
+import java.util.Optional;
+
+public interface SubscriptionCommandService {
+    Long handle(CreateSubscriptionCommand command);
+    Optional<Subscription> handle(UpdateSubscriptionCommand command);
+    void handle(DeleteSubscriptionCommand command);
+}
---
+++ src/main/java/com/glassgo/platform/paymentsandsubscriptions/services/SubscriptionQueryService.java
@@ -0,0 +1,15 @@
+package com.glassgo.platform.paymentsandsubscriptions.services;
+
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.aggregates.Subscription;
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.queries.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface SubscriptionQueryService {
+    Optional<Subscription> handle(GetSubscriptionByIdQuery query);
+    List<Subscription> handle(GetAllSubscriptionsQuery query);
+    List<Subscription> handle(GetAllSubscriptionsByPlanIdQuery query);
+    List<Subscription> handle(GetAllSubscriptionsByStatusQuery query);
+    List<Subscription> handle(GetAllSubscriptionsByUserIdQuery query);
+}

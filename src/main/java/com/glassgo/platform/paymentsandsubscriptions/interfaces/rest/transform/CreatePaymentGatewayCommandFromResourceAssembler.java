4abd98794fecf60d4f39a349ec2ea76f01c569b2
Jarod Cespedes <jarod.cespedes@gmail.com>
2025-11-18 16:04:14 -0500

    feat(rest): add assemblers for payment gateway command and resource conversions.

---
+++ src/main/java/com/glassgo/platform/paymentsandsubscriptions/interfaces/rest/transform/CreatePaymentGatewayCommandFromResourceAssembler.java
@@ -0,0 +1,23 @@
+package com.glassgo.platform.paymentsandsubscriptions.interfaces.rest.transform;
+
+import com.glassgo.platform.paymentsandsubscriptions.domain.model.commands.CreatePaymentGatewayCommand;
+import com.glassgo.platform.paymentsandsubscriptions.interfaces.rest.resources.PaymentGatewayResource;
+
+/**
+ * Assembler to convert {@link PaymentGatewayResource} to {@link CreatePaymentGatewayCommand}.
+ */
+public class CreatePaymentGatewayCommandFromResourceAssembler {
+    /**
+     * Converts {@link PaymentGatewayResource} to {@link CreatePaymentGatewayCommand}.
+     * @param resource - The payment gateway resource.
+     * @return The create payment gateway command.
+     */
+    public static CreatePaymentGatewayCommand toCommandFromResource(PaymentGatewayResource resource) {
+        return new CreatePaymentGatewayCommand(
+                resource.name(),
+                resource.apiUrl(),
+                resource.clientId(),
+                resource.secretKey()
+        );
+    }
+}

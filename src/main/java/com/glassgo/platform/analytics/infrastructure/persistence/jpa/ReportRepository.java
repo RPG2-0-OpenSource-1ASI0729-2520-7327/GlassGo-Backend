42078ba2f50e4df3afa5ce6dd601667bfb2cb579
Guillermo Howard <u202222275@upc.edu.pe>
2025-11-28 04:49:01 -0500

    feat(analytics): update reportrepository to rename method for finding reports by source id.

--- src/main/java/com/glassgo/platform/analytics/infrastructure/persistence/jpa/ReportRepository.java
+++ src/main/java/com/glassgo/platform/analytics/infrastructure/persistence/jpa/ReportRepository.java
@@ -3,11 +3,9 @@ package com.glassgo.platform.analytics.infrastructure.persistence.jpa;
 import com.glassgo.platform.analytics.domain.model.aggregates.Report;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface ReportRepository extends JpaRepository<Report, Long> {
 
-    List<Report> findAllByUserId(Long userId);
-    Optional<Report> findById(String sourceId);
+    Optional<Report> findBySourceId(String sourceId);
 }
